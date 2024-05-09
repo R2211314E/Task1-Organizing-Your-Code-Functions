d5f504ecc0c7a4d4891a9f194d680f98982b477f
R2211314E <167210964+R2211314E@users.noreply.github.com>
2024-05-09 11:02:46 +0200

    Update task_one.py
    i am done

--- task_one.py
+++ task_one.py
@@ -1,18 +1,36 @@
-def plot_data(my_file):
-    '''
-    This function plots a scatterplot provided with a coordinate text file path 
+import pandas as pd
+import matplotlib.pyplot as plt
 
-    Parameters
-    ----------
-    my_file : STRING 
-        Required path to the file with coordinates in text format 
+def get_file():
 
-    Returns
-    ---------
-    Scatterplot 
-    '''
-    scatter_plot = None 
-    """
-    ==> Write your code here <==
-    """
-    return scatter_plot 
+file_name = ("x_y_coordinates")
+try:
+  file_obj = open(file_name,"r")
+  return file_obj
+
+except FileNotFoundError:
+      print("File not found.Please try again.")
+      return get_file
+
+def plot_data(file_obj):
+
+    data = []
+    for line in file_obj:
+        values = line.strip().split()
+        if len(values)==2:
+            x,y = float(values[0]),float(values[1])
+            data.append((x,y))
+
+if data:
+ x_values, y_values = zip(*data)
+ plt.figure(figsize=(8,6))
+ plt.scatter(x_values,y_values,c="blue")
+ plt.xlabel("X")
+ plt.ylabel("Y")
+ plt.title("scatterplot of data")
+ plt.grid(True)
+ plt.legend()
+ plt.show()
+
+else:
+    print("no data found in the file.)
